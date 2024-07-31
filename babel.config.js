9bcfb4949fb94d674b3a01f415e6628a8bb758e4
Tobias Prima <primatjahja@gmail.com>
2024-07-31 23:05:58 +0800

    fix: babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-typescript",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
   ],
   plugins: ["@babel/plugin-transform-modules-commonjs"],
 };
--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,13 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   setupFilesAfterEnv: ["./src/setupTests.ts"],
   moduleNameMapper: {
-    "\\.(css|less|scss)$": "identity-obj-proxy",
+    "\\.(css|less)$": "<rootDir>/styleMock.js",
   },
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)?$": ["ts-jest"],
+    "^.+\\.(ts|tsx|js|jsx)?$": ["babel-jest"],
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/(?!axios)"],
 };
